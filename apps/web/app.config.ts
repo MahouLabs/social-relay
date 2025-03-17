8100c075f839de160c87883d71007d8a76283aa9
Nicolas Chartiot <nchartiot@proton.me>
2025-03-17 12:19:23 -0300

    Add cloudflare deploy to web app

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,8 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
+import { cloudflare } from "unenv";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
+  },
   tsr: {
     appDirectory: "src",
   },
