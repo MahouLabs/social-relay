13123f66d5899ad3ba7f304f38e9d8f3918199d1
Nicolas Chartiot <nchartiot@proton.me>
2025-03-19 11:39:37 -0300

    Remove meta logs

--- apps/web/src/routes/signin.tsx
+++ apps/web/src/routes/signin.tsx
@@ -6,7 +6,6 @@ export const Route = createFileRoute("/signin")({
 });
 
 function RouteComponent() {
-  console.log(process.env);
   return (
     <section className="flex flex-col min-h-screen items-center justify-center p-4">
       <SignIn />
--- apps/web/src/utils/auth-client.ts
+++ apps/web/src/utils/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 const baseURL = import.meta.env.VITE_AUTH_BASE_URL || "http://localhost:3000";
-console.log("Import meta env", import.meta.env);
+
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
   baseURL,
 });
