dcb6d7cb6f832713f4e00fb30433680301eacb77
Nicolas Chartiot <nchartiot@proton.me>
2025-03-20 22:37:54 -0300

    Delete old web app

--- apps/web-old/src/routes/_auth.route-a.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_auth/route-a")({
-  component: LayoutAComponent,
-});
-
-function LayoutAComponent() {
-  return <div>I'm A!</div>;
-}
