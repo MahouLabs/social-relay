f690e1af5b74a72ad2b4b1f18be2e7d0c0dd4b3a
Nicolas Chartiot <nchartiot@proton.me>
2025-03-23 10:13:10 -0300

    Delete old api

--- apps/api-old/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/d1";
-import * as schema from "./schema";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
-export const db = drizzle(process.env.DATABASE_URL, { schema });
