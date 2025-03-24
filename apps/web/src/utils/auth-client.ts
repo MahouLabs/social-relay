da93908e77310fbac4efc867d3c31f09531c4a7d
Nicolas Chartiot <nchartiot@proton.me>
2025-03-24 14:25:51 -0300

    Update to include subdomain cookies

--- apps/web/src/utils/auth-client.ts
+++ apps/web/src/utils/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // baseURL: "https://api.social-relay.com",
   baseURL: import.meta.env.VITE_API_URL,
 });
