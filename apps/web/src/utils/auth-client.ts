4118bc41026124a23f748f06c4745773f970f821
Nicolas Chartiot <nchartiot@proton.me>
2025-03-28 21:14:06 -0300

    Code cleanup

--- apps/web/src/utils/auth-client.ts
+++ apps/web/src/utils/auth-client.ts
@@ -2,7 +2,6 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_API_URL,
-	// baseURL: "http://localhost:5173",
 	basePath: "/auth",
 	fetchOptions: {
 		credentials: "include",
