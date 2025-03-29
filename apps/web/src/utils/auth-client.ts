4882a10fd52d407ea8d02c5ee8290ee928263742
Nicolas Chartiot <nchartiot@proton.me>
2025-03-28 21:13:10 -0300

    Use env var for api url

--- apps/web/src/utils/auth-client.ts
+++ apps/web/src/utils/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	// baseURL: import.meta.env.VITE_API_URL,
-	baseURL: "http://localhost:5173",
+	baseURL: import.meta.env.VITE_API_URL,
+	// baseURL: "http://localhost:5173",
 	basePath: "/auth",
 	fetchOptions: {
 		credentials: "include",
