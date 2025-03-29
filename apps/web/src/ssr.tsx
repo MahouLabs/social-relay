38967544f98dda480aeea4abc686ddf9c82163e8
Nicolas Chartiot <nchartiot@proton.me>
2025-03-28 21:10:37 -0300

    Format code

--- apps/web/src/ssr.tsx
+++ apps/web/src/ssr.tsx
@@ -1,13 +1,13 @@
 /// <reference types="vinxi/types/server" />
 import { getRouterManifest } from "@tanstack/react-start/router-manifest";
 import {
-  createStartHandler,
-  defaultStreamHandler,
+	createStartHandler,
+	defaultStreamHandler,
 } from "@tanstack/react-start/server";
 
 import { createRouter } from "./router";
 
 export default createStartHandler({
-  createRouter,
-  getRouterManifest,
+	createRouter,
+	getRouterManifest,
 })(defaultStreamHandler);
